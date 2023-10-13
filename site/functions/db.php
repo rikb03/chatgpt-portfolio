7397340a7cedf65496e8bbc70d28f2082815d173
rikb03 <rikbakker03@gmail.com>
2023-10-13 13:46:28 +0200

    added DB functions

--- site/functions/db.php
+++ site/functions/db.php
@@ -1,42 +1,39 @@
 <?php
-
+$db = new db();
 class db{
     private function getDBParams(){
         $app = require 'private.php';
         $dbconn = $app['database'];
         return $dbconn;
     }
-    function execSQL($sql){
+    function SQLConnectDB(){
+        $dbconn = $this->getDBParams();
+        $conn = new PDO("mysql:host=$dbconn[servername];dbname=$dbconn[db]", $dbconn['username'], $dbconn['password']);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+    function SQLSelect($sql){
         try {
-            $dbconn = $this->getDBParams();
-            $conn = new PDO("mysql:host=$dbconn[servername];dbname=$dbconn[db]", $dbconn['username'], $dbconn['password']);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo "Connected successfully";
-            global $results;
+            $conn = $this->SQLConnectDB();
             $stmt = $conn->prepare($sql);
             $stmt->execute();
+            global $results;
             $results = $stmt->fetchall(PDO::FETCH_ASSOC);
           } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
           }
-        
+        $conn = null;
     }
-    function selectParamsSQL($sql, $param, $param2){
+    function SQLInsert($sql){
         try {
-            $dbconn = $this->getDBParams();
-            $conn = new PDO("mysql:host=$dbconn[servername];dbname=$dbconn[db]", $dbconn['username'], $dbconn['password']);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo "Connected successfully";
-            global $results;
+            $conn = $this->SQLConnectDB();
             $stmt = $conn->prepare($sql);
-            $stmt->bindParam($param, $param2);
             $stmt->execute();
-            $results = $stmt->fetchall(PDO::FETCH_ASSOC);
-          } catch(PDOException $e) {
+        } catch(PDOException $e){
             echo "Connection failed: " . $e->getMessage();
-          }
-        
+        }
+        $conn = null;
     }
+        
 }
