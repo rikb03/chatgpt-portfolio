1b668fe66b063ccdc715cd9257eb683cc066b192
rikb03 <rikbakker03@gmail.com>
2023-10-16 13:36:09 +0200

    Added comments

--- site/functions/routes.php
+++ site/functions/routes.php
@@ -1,7 +1,7 @@
 <?php
-
+// Function to route to the correct controller based on the URL
 function routing(){
-
+    // Array with routes
     $routes = [
         "/" => "controllers/index.php",
         "/404" => "controllers/404.php",
@@ -11,7 +11,7 @@ function routing(){
         "/changepassword" => "controllers/passwordchange.php",
         "/admin" => "controllers/admin.php",
     ];
-
+    // Check if the URL exists in the array and require the correct controller else require the 404 controller
     if(array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
         require $routes[$_SERVER['REQUEST_URI']];
     } else {
