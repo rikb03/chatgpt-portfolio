0687ba24c71025a27ac971e815f64ffde93b189c
Rik Bakker <rikbakker03@gmail.com>
2023-10-17 11:54:39 +0200

    Removed function bracket, changed 404 to http response

--- site/functions/routes.php
+++ site/functions/routes.php
@@ -1,25 +1,25 @@
 <?php
 // Function to route to the correct controller based on the URL
-function routing(){
-    // Array with routes
-    $routes = [
-        "/" => "controllers/index.php",
-        "/404" => "controllers/404.php",
-        "/details" => "controllers/details.php",
-        "/login" => "controllers/login.php",
-        "/edit" => "controllers/userdetails.php",
-        "/changepassword" => "controllers/passwordchange.php",
-        "/admin" => "controllers/admin.php",
-    ];
-    // Check if the URL exists in the array and require the correct controller else require the 404 controller
-    if(array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
-        require $routes[$_SERVER['REQUEST_URI']];
-    } 
-    elseif(str_contains($_SERVER['REQUEST_URI'], "?id")){
-        require $routes["/details"];
-    }
-    else {
-        // echo "404";
-        require $routes['/404'];
-    };
+// Array with routes
+$routes = [
+    "/" => "controllers/index.php",
+    "/404" => "controllers/404.php",
+    "/details" => "controllers/details.php",
+    "/login" => "controllers/login.php",
+    "/edit" => "controllers/userdetails.php",
+    "/changepassword" => "controllers/passwordchange.php",
+    "/admin" => "controllers/admin.php",
+];
+// Check if the URL exists in the array and require the correct controller else require the 404 controller
+if(array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
+    require $routes[$_SERVER['REQUEST_URI']];
+} 
+elseif(str_contains($_SERVER['REQUEST_URI'], "?id")){
+    require $routes["/details"];
 }
+else {
+    // echo "404";
+    // require $routes['/404'];
+    http_response_code(404);
+};
+?>
