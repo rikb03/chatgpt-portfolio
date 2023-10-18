9837d66d73ea5dad25b1ca6f797774626e9494bb
Rik Bakker <rikbakker03@gmail.com>
2023-10-18 12:32:26 +0200

    Added comments, changed elseif to catch all the URL with "?id"

--- site/functions/routes.php
+++ site/functions/routes.php
@@ -9,15 +9,20 @@ $routes = [
     "/edit" => "controllers/userdetails.php",
     "/changepassword" => "controllers/passwordchange.php",
     "/admin" => "controllers/admin.php",
+    "/test" => "test.php"
 ];
 // Check if the URL exists in the array and require the correct controller else require the 404 controller
 if(array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
+    // If the URL exists in the array require the correct controller
     require $routes[$_SERVER['REQUEST_URI']];
 } 
 elseif(str_contains($_SERVER['REQUEST_URI'], "?id")){
-    require $routes["/details"];
+    // If the URL contains '?id' remove it from the URL and require the correct controller
+    $uri = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?id"));
+    require $routes[$uri];
 }
 else {
+    // If the URL doesn't exist in the array throw a 404 error
     // echo "404";
     // require $routes['/404'];
     http_response_code(404);
