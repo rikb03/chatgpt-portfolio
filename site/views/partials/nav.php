ad79945c573b22f10ecae4b9f8f7c67a03724bdd
Rik Bakker <rikbakker03@gmail.com>
2023-10-20 11:38:51 +0200

    Removed php tag from nav.php

--- site/views/partials/nav.php
+++ site/views/partials/nav.php
@@ -1,23 +1,18 @@
-<?php
-echo
-'       <nav class="navbar">
-            <div class="local">
-                <h2>Profile App</h2>
-            </div>
-            <ul class="nav-menu">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Login</a>
-                </li>
-            </ul>
-            <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-        </nav>
-        ';
-?>
-
+<nav class="navbar">
+    <div class="local">
+        <h2>Profile App</h2>
+    </div>
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Login</a>
+        </li>
+    </ul>
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+</nav>
