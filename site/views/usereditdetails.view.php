5df1f24ffb05a0d44433b79936324c22c7b00371
AshrafBasnoe-Github <s1185527@student.windesheim.nl>
2023-10-26 18:37:49 +0200

    useredit

---
+++ site/views/usereditdetails.view.php
@@ -0,0 +1,13 @@
+<?php
+require '../functions/db.php';
+echo "<head><link rel='stylesheet' href='../public/styles/mainstyle.css'></head>";
+require '../views/partials/nav.php';
+echo "<h1> Edit User </h1>";
+$id = $_GET["id"];
+$db = new db;
+$query = "SELECT user.id, concat(firstName, ' ', lastName) AS Naam, description AS Beschrijving FROM user WHERE id = :id";
+$result = $db->SQLReturnResultWithParams($query, ":id", $id);
+foreach($result as $user){
+    echo $user["Naam"] . "<br>" . $user["Beschrijving"];
+}
+?>
