d609e838470ea9276a1bf72524ee8b1d7aef41e3
Rik Bakker <rikbakker03@gmail.com>
2023-11-01 08:38:12 +0100

    Added unset for session['error/method'] so there are no trailing variable's

--- site/controllers/login.php
+++ site/controllers/login.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
-require 'views/login.view.php';
-
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     // If the user is already logged in redirect to the homepage(Maybe change this to the profile page)
+    unset($_SESSION['error']);
+    unset($_SESSION['method']);
     header('Location: /');
-} elseif ($_SESSION['error'] && $_SESSION['method'] == 'login') {
+} else {
+    // If the user is not logged in show the login page
+    require 'views/login.view.php';
+}
+if ($_SESSION['error'] && $_SESSION['method'] == 'login') {
     // If the user is not logged in, but there is an error, show the error message
     // echo 'Wachtwoord of gebruikersnaam klopt niet';
     echo '<script type="text/javascript">errorLogin("*'.$_SESSION['errorMessage'].'");</script>';
--- site/controllers/passwordchange.php
+++ site/controllers/passwordchange.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-// print("<pre>".print_r($_SESSION,true)."</pre>");
 require 'views/passwordchange.view.php';
 if ($_SESSION['error'] && $_SESSION['method'] == 'passwordchange') {
     // If there is an error, show the error message
