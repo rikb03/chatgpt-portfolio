20d32a0e2db5a5b919fcd17ab4da99a2214aa97e
Rik Bakker <rikbakker03@gmail.com>
2023-11-03 00:05:15 +0100

    Changed function errormessage to accept different header location, default is ['HTTP_REFERER']

--- site/functions/errorMessage.php
+++ site/functions/errorMessage.php
@@ -1,9 +1,10 @@
 <?php
 // Sets the error message for login/signup/passwordchange and redirects to the previous page
-function errorMessage($message){
+function errorMessage($message, $location = NULL){
+    $location = $location ?? $_SERVER['HTTP_REFERER'];
     $_SESSION['method'] = $_POST["method"];
     $_SESSION['error'] = TRUE;
     $_SESSION['errorMessage'] = $message;
-    header ('Location: '. $_SERVER['HTTP_REFERER']);
+    header ('Location: '. $location);
     exit();
 }
